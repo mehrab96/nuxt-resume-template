c51aadbaa34dffbd0f3ec1e43dac32ef4c1a4a51
mehrab esmailnia <mehrabesmailnia96@gmail.com>
2024-07-01 00:03:00 +0330

    index page has been added and responsive is done

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,148 @@
 <template>
 
 <NuxtLayout name="master">
-    index page
+    <div class="rounded-3xl z-20 max-lg:mt-8">
+        <div class="bg-white sticky top-0 px-6 max-xl:px-2 rounded-3xl py-10">
+            <figure class="w-44 h-44 overflow-hidden m-auto rounded-[1.5rem]">
+                <img src="/images/MehrabEsmailnia.jpeg" class="w-full h-full object-cover" alt="">
+            </figure>
+            <h1 class="text-center text-primary-active p-4 font-black text-xl">MehrabEsmailnia</h1>
+            <div class="text-[.82rem] font-light text-center grid justify-center">
+                <span class="bg-[#f6f6f6] text-primary-active rounded-xl py-3 px-8">Web Developer</span>
+            </div>
+            <div class="bg-light mt-6 rounded-3xl">
+                <div class="grid grid-cols-[repeat(4,auto)] px-10 gap-4 justify-center h-20 items-center">
+                    <a href="https://t.me/programmer_meh" target="_blank" class="bg-primary-active w-8 h-8 rounded-[.5rem] grid items-center justify-center">
+                        <i
+                        class=
+                        "fi fi-brands-telegram text-white flex"
+                        ></i>
+                    </a>
+                    <a href="https://github.com/mehrab96" target="_blank" class="bg-primary-active w-8 h-8 rounded-[.5rem] grid items-center justify-center "><i
+                        class=
+                        "fi fi-brands-github text-white flex"
+                        ></i></a>
+                    <a href="https://wa.me/+989112176238" target="_blank" class="bg-primary-active w-8 h-8 rounded-[.5rem] grid items-center justify-center "><i
+                        class=
+                        "fi fi-brands-whatsapp text-white flex"
+                        ></i></a>
+                    <a href="https://www.instagram.com/m.mehrab76" target="_blank" class="bg-primary-active w-8 h-8 rounded-[.5rem] grid items-center justify-center "><i
+                        class=
+                        "fi fi-brands-instagram text-white flex"
+                        ></i></a>
+                </div>
+                <ul class="px-10 max-xl:px-6 grid gap-2 text-md font-light text-primary-active">
+                    <li class="grid truncate gap-2 h-8 justify-start items-center grid-cols-[auto_auto]">
+                        <div class="grid items-center">
+                            <i class="fi fi-rr-calendar-lines text-xl flex"></i>
+                        </div>
+                        <span>1997/12/20</span>
+                    </li>
+                    <li class="grid gap-2 h-8 justify-start items-center grid-cols-[auto_auto]">
+                        <div class="grid items-center">
+                            <i class="fi fi-rr-visit flex text-xl"></i>
+                        </div>
+                        <span>Babol, Mazandaran, Iran</span>
+                    </li>
+                    <li class="grid gap-2 h-8 justify-start items-center grid-cols-[auto_auto]">
+                        <div class="grid items-center">
+                            <i class="fi fi-rr-envelope flex text-xl"></i>
+                        </div>
+                        <span>mehrabesmailnia96[@]gmail.com</span>
+                    </li>
+                    <li class="grid gap-2 h-8 justify-start items-center grid-cols-[auto_auto]">
+                        <div class="grid items-center">
+                            <i class="fi fi-rr-phone-call flex text-xl"></i>
+                        </div>
+                        <span>+980112176238</span>
+                    </li>
+                </ul>
+                <div class="grid justify-center py-5">
+                    <a href="./images/MehrabEsmailnia.pdf" download class="bg-primary-active text-sm text-light font-semibold shadow-xl shadow-primary-medium/40 px-12 rounded-full py-3">
+                        <span>Download resume</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="px-2 mb-10 mt-10">
+    <div class="highlight">
+                <span class="font-black text-xl text-primary-active relative">About me</span>
+                <p class="font-extralight ltr:font-light text-base text-primary-active text-justify leading-9 opacity-90">I am a senior software engineering expert with a strong foundation in various programming languages and web development frameworks.
+
+                    Over the past five years, I have specialized in using Laravel for backend development and Nuxt.js for frontend development. My expertise in Laravel has allowed me to build robust, scalable web applications, while my proficiency in Nuxt.js has enabled me to create highly performant, server-side rendered applications. Additionally, I have extensive experience in building admin panels using Laravel and Vue.js, creating intuitive and efficient interfaces for managing complex systems.</p>
+        </div>
+        <div class="py-6">
+            <div class="highlight">
+                <span class="font-black text-primary-active text-xl relative">Field of activity</span>
+            </div>
+            <section class="grid grid-cols-2 max-sm:grid-cols-1 gap-4 mt-4">
+                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
+                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/layers.png" alt="Web developer"></figure>
+                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">Web developer</h2>
+                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Full stack website implementation (spa, mpa)</p>
+                </article>
+                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
+                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/api.png" alt="Web developer"></figure>
+                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">Back-end developer</h2>
+                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Service implementation for different platforms (Android, iOS, Windows, web application)</p>
+                </article>
+                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
+                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/web-design.png" alt="Web developer"></figure>
+                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">Front-end developer</h2>
+                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Using JavaScript framework to develop applications (ssr, spa)</p>
+                </article>
+                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
+                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/seo.png" alt="Web developer"></figure>
+                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">SEO specialist</h2>
+                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Activities to improve ranking and receive organic traffic in search engine results</p>
+                </article>
+            </section>
+        </div>
+        <div class="py-6">
+            <div class="highlight">
+                <span class="font-black text-black/80 text-xl relative">My skills</span>
+            </div>
+            <section class="grid grid-cols-1 gap-4 mt-4">
+                <article class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 90%;">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Laravel</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%90</span>
+                    </div>
+                </article>
+                <article class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 90%;">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Vue js</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%90</span>
+                    </div>
+                </article>
+                <article class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 70%;">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">NuxtJs</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%70</span>
+                    </div>
+                </article>
+                <article class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 80%;">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Typescript</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%80</span>
+                    </div>
+                </article>
+                <article class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 95%;">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">TailwindCss</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%95</span>
+                    </div>
+                </article>
+                <article class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 80%;">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Git and Github</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%80</span>
+                    </div>
+                </article>
+            </section>
+        </div>
+    </div>
 
 </NuxtLayout>
 
