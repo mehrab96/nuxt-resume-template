44344d28afb5fb9f32fbd6ba509e637eea28d41f
mehrab esmailnia <mehrabesmailnia96@gmail.com>
2024-07-01 18:36:10 +0330

    active link in navbar has been done

--- layouts/master.vue
+++ layouts/master.vue
@@ -9,10 +9,10 @@
              before:absolute before:bg-contain before:top-0 before:-right-[9.5rem] before:bg-svgTopHead before:w-full">
              <nav class="h-[4rem] mt-6 pt-3 max-mini:h-[4rem] max-mini:pt-0 z-20 rounded-2xl bg-light/70 items-center grid grid-cols-[repeat(4,auto)] webkit-inline-box
              gap-8 max-mini:gap-2 justify-center">
-                <NuxtLink class="activeUrl z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" to="/">About me</NuxtLink>
-                <NuxtLink class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" to="/resume">Resume</NuxtLink>
-                <NuxtLink class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" to="/work-samples">Work samples</NuxtLink>
-                <NuxtLink class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" to="/contact-us">Contact me</NuxtLink>
+                <NuxtLink :class="{'activeUrl' : $route.name == 'index'}" class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" to="/">About me</NuxtLink>
+                <NuxtLink class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" :class="{'activeUrl' : $route.name == 'resume'}" to="/resume">Resume</NuxtLink>
+                <NuxtLink class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" :class="{'activeUrl' : $route.name == 'work-samples'}" to="/work-samples">Work samples</NuxtLink>
+                <NuxtLink class="z-10 max-minixl:px-4 max-mini:px-5 max-md:px-6 max-sm:text-xs text-primary-active max-lg:px-8 max-xl:px-6 px-9 h-10 grid items-center text-sm" :class="{'activeUrl' : $route.name == 'contact-us'}" to="/contact-us">Contact me</NuxtLink>
             </nav>
         </div>
 
