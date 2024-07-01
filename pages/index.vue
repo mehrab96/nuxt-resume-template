d0bbf3ba4ab6312ca2af682471a507b72caec049
mehrab esmailnia <mehrabesmailnia96@gmail.com>
2024-07-01 18:52:49 +0330

    The index page is dynamic.

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <script setup>
+const bio = ref('');
+const fieldActivity = ref([]);
+const skills = ref([]);
+$fetch('/data/about-me.json').then(res => {
+    bio.value = res.bio;
+    fieldActivity.value = res.fieldActivity;
+    skills.value = res.skills;
+});
+
 
 </script>
 
@@ -8,34 +17,17 @@
     <div class="px-2 mb-10 mt-10">
         <div class="highlight">
                 <span class="font-black text-xl text-primary-active relative">About me</span>
-                <p class="font-extralight ltr:font-light text-base text-primary-active text-justify leading-9 opacity-90">I am a senior software engineering expert with a strong foundation in various programming languages and web development frameworks.
-
-                    Over the past five years, I have specialized in using Laravel for backend development and Nuxt.js for frontend development. My expertise in Laravel has allowed me to build robust, scalable web applications, while my proficiency in Nuxt.js has enabled me to create highly performant, server-side rendered applications. Additionally, I have extensive experience in building admin panels using Laravel and Vue.js, creating intuitive and efficient interfaces for managing complex systems.</p>
+                <p class="font-extralight ltr:font-light text-base text-primary-active text-justify leading-9 opacity-90">{{ bio }}</p>
         </div>
         <div class="py-6">
             <div class="highlight">
                 <span class="font-black text-primary-active text-xl relative">Field of activity</span>
             </div>
             <section class="grid grid-cols-2 max-sm:grid-cols-1 gap-4 mt-4">
-                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
-                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/layers.png" alt="Web developer"></figure>
-                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">Web developer</h2>
-                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Full stack website implementation (spa, mpa)</p>
-                </article>
-                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
-                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/api.png" alt="Web developer"></figure>
-                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">Back-end developer</h2>
-                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Service implementation for different platforms (Android, iOS, Windows, web application)</p>
-                </article>
-                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
-                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/web-design.png" alt="Web developer"></figure>
-                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">Front-end developer</h2>
-                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Using JavaScript framework to develop applications (ssr, spa)</p>
-                </article>
-                <article class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
-                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" src="/images/seo.png" alt="Web developer"></figure>
-                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">SEO specialist</h2>
-                    <p class="text-center px-12 font-light text-sm text-primary-active/70">Activities to improve ranking and receive organic traffic in search engine results</p>
+                <article v-for="(field , index) in fieldActivity" :key="index" class="h-52 grid justify-center content-center bg-light-white shadow-[0_0_20px] shadow-gray-600/10 rounded-3xl">
+                    <figure class="grid justify-center h-16"><img class="w-full h-16 object-contain" :src="field.image" :alt="field.title"></figure>
+                    <h2 class="text-center font-black mt-3 pb-1 text-xl text-primary-active">{{field.title}}</h2>
+                    <p class="text-center px-12 font-light text-sm text-primary-active/70">{{ field.explanation }}</p>
                 </article>
             </section>
         </div>
@@ -44,40 +36,10 @@
                 <span class="font-black text-black/80 text-xl relative">My skills</span>
             </div>
             <section class="grid grid-cols-1 gap-4 mt-4">
-                <article class="w-full h-10 bg-gray-200 rounded-full">
-                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 90%;">
-                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Laravel</span>
-                        <span class="absolute right-6 top-0 font-semibold z-30">%90</span>
-                    </div>
-                </article>
-                <article class="w-full h-10 bg-gray-200 rounded-full">
-                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 90%;">
-                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Vue js</span>
-                        <span class="absolute right-6 top-0 font-semibold z-30">%90</span>
-                    </div>
-                </article>
-                <article class="w-full h-10 bg-gray-200 rounded-full">
-                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 70%;">
-                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">NuxtJs</span>
-                        <span class="absolute right-6 top-0 font-semibold z-30">%70</span>
-                    </div>
-                </article>
-                <article class="w-full h-10 bg-gray-200 rounded-full">
-                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 80%;">
-                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Typescript</span>
-                        <span class="absolute right-6 top-0 font-semibold z-30">%80</span>
-                    </div>
-                </article>
-                <article class="w-full h-10 bg-gray-200 rounded-full">
-                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 95%;">
-                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">TailwindCss</span>
-                        <span class="absolute right-6 top-0 font-semibold z-30">%95</span>
-                    </div>
-                </article>
-                <article class="w-full h-10 bg-gray-200 rounded-full">
-                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" style="width: 80%;">
-                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">Git and Github</span>
-                        <span class="absolute right-6 top-0 font-semibold z-30">%80</span>
+                <article v-for="(skill , index) in skills" :key="index" class="w-full h-10 bg-gray-200 rounded-full">
+                    <div class="relative overflow-hidden transition-all duration-[3000ms] h-full leading-[2.5rem] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-full" :style="{width: skill.percent + '%'}">
+                        <span class="absolute left-6 top-0 z-30 text-sm leading-[2.4rem] font-semibold">{{skill.title}}</span>
+                        <span class="absolute right-6 top-0 font-semibold z-30">%{{skill.percent}}</span>
                     </div>
                 </article>
             </section>
