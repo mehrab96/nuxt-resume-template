a5b38ddae2eb24f38a109697b319d85b69492ae4
mehrab esmailnia <mehrabesmailnia96@gmail.com>
2024-07-02 15:02:33 +0330

    The work samples page has been done

--- pages/work-samples.vue
+++ pages/work-samples.vue
@@ -1,4 +1,9 @@
 <script setup>
+const samples = ref([])
+$fetch('/data/work-samples.json').then(res => {
+    samples.value = res.workSamples;
+});
+
 
 </script>
 
@@ -6,59 +11,19 @@
 <NuxtLayout name="master">
     <div class="px-2 mb-10 mt-10">
                 <section class="grid mt-6 grid-cols-2 max-sm:grid-cols-1 gap-6 ltr:text-left ">
-                    <article>
+                    <article v-for="(sample , index) in samples" :key="index">
                         <figure class="h-52 relative group overflow-hidden border-[1px] rounded-xl">
-                            <a href="./project-number-one.html" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
+                            <NuxtLink to="/samples" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
                                 <span>View the Zoom information</span>
                                 <i class="fi fi-br-arrow-right flex ml-2 mt-1"></i>
-                            </a>
-                            <img class="w-full object-cover h-full" src="/images/image-not-found.webp" alt="">
+                            </NuxtLink>
+                            <img v-if="!sample.image" class="w-full object-cover h-full" src="/images/image-not-found.webp" :alt="sample.title">
+                            <img v-else class="w-full object-cover h-full" :src="sample.image" :alt="sample.title">
                         </figure>
-                    </article>
-                    <article>
-                        <figure class="h-52 relative group overflow-hidden border-[1px] rounded-xl">
-                            <a href="./project-number-one.html" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
-                                <span>View the Zoom information</span>
-                                <i class="fi fi-br-arrow-right flex ml-2 mt-1"></i>
-                            </a>
-                            <img class="w-full object-cover h-full" src="/images/image-not-found.webp" alt="">
-                        </figure>
-                    </article>
-                    <article>
-                        <figure class="h-52 relative group overflow-hidden border-[1px] rounded-xl">
-                            <a href="./project-number-one.html" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
-                                <span>View the Zoom information</span>
-                                <i class="fi fi-br-arrow-right flex ml-2 mt-1"></i>
-                            </a>
-                            <img class="w-full object-cover h-full" src="/images/image-not-found.webp" alt="">
-                        </figure>
-                    </article>
-                    <article>
-                        <figure class="h-52 relative group overflow-hidden border-[1px] rounded-xl">
-                            <a href="./project-number-one.html" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
-                                <span>View the Zoom information</span>
-                                <i class="fi fi-br-arrow-right flex ml-2 mt-1"></i>
-                            </a>
-                            <img class="w-full object-cover h-full" src="/images/image-not-found.webp" alt="">
-                        </figure>
-                    </article>
-                    <article>
-                        <figure class="h-52 relative group overflow-hidden border-[1px] rounded-xl">
-                            <a href="./project-number-one.html" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
-                                <span>View the Zoom information</span>
-                                <i class="fi fi-br-arrow-right flex ml-2 mt-1"></i>
-                            </a>
-                            <img class="w-full object-cover h-full" src="/images/image-not-found.webp" alt="">
-                        </figure>
-                    </article>
-                    <article>
-                        <figure class="h-52 relative group overflow-hidden border-[1px] rounded-xl">
-                            <a href="./project-number-one.html" class="flex rtl:flex-row-reverse ltr:flex-row-reverse items-center justify-center text-light font-semibold absolute opacity-0 visibility-hidden transition-all duration-500 hover:opacity-100 group-hover:visibility-visible top-0 right-0 w-full h-full bg-primary-active/80">
-                                <span>View the Zoom information</span>
-                                <i class="fi fi-br-arrow-right flex ml-2 mt-1"></i>
-                            </a>
-                            <img class="w-full object-cover h-full" src="/images/image-not-found.webp" alt="">
-                        </figure>
+                        <div class="p-2">
+                            <h3 class="font-semibold">{{ sample.title }}</h3>
+                            <p class="text-xs">{{ sample.description }}</p>
+                        </div>
                     </article>
                 </section>
             </div>
